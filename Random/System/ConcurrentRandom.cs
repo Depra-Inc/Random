47d80623570ef30617f7247ed7f6530472174795
godzzz <g0dzZz1lla@yandex.ru>
2022-10-13 00:25:39 +0300

    ConcurrentRandom refactored

---
+++ Random/System/ConcurrentRandom.cs
@@ -0,0 +1,89 @@
+using System;
+
+namespace Depra.Random.System
+{
+    public sealed class ConcurrentRandom : global::System.Random
+    {
+        private static readonly global::System.Random IMPL;
+
+        public override int Next() => Next(1, int.MaxValue);
+
+        public override int Next(int maxExclusive) => Next(1, maxExclusive);
+
+        public override int Next(int minInclusive, int maxExclusive) => IMPL.Next(minInclusive, maxExclusive);
+
+        public override double NextDouble() => IMPL.NextDouble();
+
+        public override void NextBytes(byte[] buffer) => IMPL.NextBytes(buffer);
+
+        static ConcurrentRandom()
+        {
+            IMPL =
+#if NET6_0_OR_GREATER
+                new global::System.Random();
+#else
+                new ConcurrentRandomImpl();
+#endif
+        }
+
+        private class ConcurrentRandomImpl : global::System.Random
+        {
+            private static readonly global::System.Random GLOBAL = new global::System.Random();
+
+            [ThreadStatic] private static global::System.Random _local;
+
+            public override int Next() => Next(1, int.MaxValue);
+
+            public override int Next(int maxExclusive) => Next(1, maxExclusive);
+
+            public override int Next(int minInclusive, int maxExclusive)
+            {
+                var inst = _local;
+                if (inst != null)
+                {
+                    return inst.Next(minInclusive, maxExclusive);
+                }
+                
+                _local = inst = CreateRandom();
+                return inst.Next(minInclusive, maxExclusive);
+            }
+
+            public override double NextDouble()
+            {
+                var inst = _local;
+                if (inst != null)
+                {
+                    return inst.NextDouble();
+                }
+                
+                _local = inst = CreateRandom();
+                return inst.NextDouble();
+            }
+
+            public override void NextBytes(byte[] buffer)
+            {
+                var inst = _local;
+                if (inst != null)
+                {
+                    inst.NextBytes(buffer);
+                    return;
+                }
+                
+                _local = inst = CreateRandom();
+                inst.NextBytes(buffer);
+            }
+
+            private static global::System.Random CreateRandom()
+            {
+                int seed;
+
+                lock (GLOBAL)
+                {
+                    seed = GLOBAL.Next();
+                }
+                
+                return new global::System.Random(seed);
+            }
+        }
+    }
+}
