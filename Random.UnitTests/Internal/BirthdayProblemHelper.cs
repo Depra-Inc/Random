60f53ed3628935d67d5d2354e9049f13c513e636
godzzz <g0dzZz1lla@yandex.ru>
2022-10-12 23:15:17 +0300

    UnitTests updated
    Test WhenGettingOneThousandRandomValues_ThenNoDuplicateValuesFound (SystemIntegerRandomizersTests.cs) removed (too hard)

--- Random.UnitTests/Internal/BirthdayProblemHelper.cs
+++ Random.UnitTests/Internal/BirthdayProblemHelper.cs
@@ -5,6 +5,14 @@ namespace Depra.Random.UnitTests.Internal
     internal static class BirthdayProblemHelper
     {
         /// <summary>
+        /// The expected total number of times a selection will repeat a previous selection as n such values.
+        /// </summary>
+        /// <param name="n">Number of possible values.</param>
+        /// <param name="k">Number of samples.</param>
+        /// <returns></returns>
+        public static double FindCollisionsCount(double n, double k) => k - n + n * Math.Pow((n - 1) / n, k);
+
+        /// <summary>
         /// Returns collision probability in <see cref="k"/> samples,
         /// each of which is a random selection of <see cref="n"/> possible values.
         /// </summary>
@@ -18,9 +26,49 @@ namespace Depra.Random.UnitTests.Internal
         /// Returns the approximate number of values for a given probability.
         /// </summary>
         /// <param name="n">Number of possible values.</param>
-        /// <param name="p">Collision probability</param>
+        /// <param name="p">Collision probability.</param>
         /// <returns></returns>
         public static double GetApproximateNumberOfValuesForProbability(double n, double p) =>
             Math.Ceiling(Math.Sqrt(2 * n * Math.Log(1 / (1 - p))));
+
+        public static double Test(double n, double k)
+        {
+            double pr = 0.7;
+            int result = 0;
+
+            double p = 1;
+            while (p > pr)
+            {
+                p *= (n / k);
+                n--;
+                result++;
+            }
+
+            //Console.Write("\nTotal no. of people out of which there is ");
+            //Console.Write("{0:F1} probability that two of them have same birthdays is {1} ", p, result);
+
+            return result;
+        }
+
+        public static double QM(double m)
+        {
+            double sum = 0;
+            for (var k = 1; k <= m; k++)
+            {
+                sum += Factorial(m) / Factorial(m - k) * (1 / Math.Pow(m, k));
+            }
+
+            return sum;
+        }
+
+        public static double Factorial(double n)
+        {
+            if (n == 1) 
+            { 
+                return 1; 
+            }
+
+            return n * Factorial(n - 1);
+        }
     }
 }
--- Random.UnitTests/SystemIntegerRandomizersTests.cs
+++ Random.UnitTests/SystemIntegerRandomizersTests.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Depra.Random.Extensions;
 using Depra.Random.Randomizers;
@@ -11,16 +10,16 @@ using NUnit.Framework;
 
 namespace Depra.Random.UnitTests
 {
-    [TestFixture(sbyte.MinValue, sbyte.MaxValue, sbyte.MinValue + sbyte.MaxValue, TypeArgs = new[] {typeof(sbyte)})]
-    [TestFixture(byte.MinValue, byte.MaxValue, byte.MinValue + byte.MaxValue, TypeArgs = new[] {typeof(byte)})]
-    [TestFixture(short.MinValue, short.MaxValue, short.MinValue + short.MaxValue, TypeArgs = new[] {typeof(short)})]
-    [TestFixture(ushort.MinValue, ushort.MaxValue, ushort.MinValue + ushort.MaxValue, TypeArgs = new[] {typeof(ushort)})]
-    [TestFixture(int.MinValue, int.MaxValue, int.MinValue + int.MaxValue, TypeArgs = new[] {typeof(int)})]
-    [TestFixture(uint.MinValue, uint.MaxValue, uint.MinValue + uint.MaxValue, TypeArgs = new[] {typeof(uint)})]
-    [TestFixture(long.MinValue, long.MaxValue, long.MinValue + long.MaxValue, TypeArgs = new[] {typeof(long)})]
-    [TestFixture(ulong.MinValue, ulong.MaxValue, ulong.MinValue + ulong.MaxValue, TypeArgs = new[] {typeof(ulong)})]
-    [TestFixture(float.MinValue, float.MaxValue, float.MinValue + float.MaxValue, TypeArgs = new[] {typeof(float)})]
-    [TestFixture(double.MinValue, double.MaxValue, double.MinValue + double.MaxValue, TypeArgs = new[] {typeof(double)})]
+    [TestFixture(sbyte.MinValue, sbyte.MaxValue, sbyte.MinValue + sbyte.MaxValue, TypeArgs = new[] { typeof(sbyte) })]
+    [TestFixture(byte.MinValue, byte.MaxValue, byte.MinValue + byte.MaxValue, TypeArgs = new[] { typeof(byte) })]
+    [TestFixture(short.MinValue, short.MaxValue, short.MinValue + short.MaxValue, TypeArgs = new[] { typeof(short) })]
+    [TestFixture(ushort.MinValue, ushort.MaxValue, ushort.MinValue + ushort.MaxValue, TypeArgs = new[] { typeof(ushort) })]
+    [TestFixture(int.MinValue, int.MaxValue, int.MinValue + int.MaxValue, TypeArgs = new[] { typeof(int) })]
+    [TestFixture(uint.MinValue, uint.MaxValue, uint.MinValue + uint.MaxValue, TypeArgs = new[] { typeof(uint) })]
+    [TestFixture(long.MinValue, long.MaxValue, long.MinValue + long.MaxValue, TypeArgs = new[] { typeof(long) })]
+    [TestFixture(ulong.MinValue, ulong.MaxValue, ulong.MinValue + ulong.MaxValue, TypeArgs = new[] { typeof(ulong) })]
+    [TestFixture(float.MinValue, float.MaxValue, float.MinValue + float.MaxValue, TypeArgs = new[] { typeof(float) })]
+    [TestFixture(double.MinValue, double.MaxValue, double.MinValue + double.MaxValue, TypeArgs = new[] { typeof(double) })]
     public class SystemIntegerRandomizersTests<T> where T : IComparable<T>
     {
         private readonly T _minValue;
@@ -58,39 +57,6 @@ namespace Depra.Random.UnitTests
             // Assert.
             randomValue.Should().BeInRange(minValue, maxValue);
         }
-
-        [Test]
-        public void WhenGettingOneThousandRandomValues_ThenNoDuplicateValuesFound(
-            [ValueSource(nameof(GetRandomizers))] INumberRandomizer<T> randomizer)
-        {
-            // Arrange.
-            const int valuesCount = 1000;
-            var randomValues = new T[valuesCount];
-
-            // Act.
-            for (var i = 0; i < randomValues.Length; i++)
-            {
-                randomValues[i] = randomizer.Next();
-            }
-
-            // TODO: Finalize
-            var collisionsCount = FindCollisionsCount(randomValues);
-            var collisionsProbability = collisionsCount / (double) valuesCount;
-            var expectedCollisionProbability =
-                BirthdayProblemHelper.GetCollisionProbability(_numberOfPossibleValues, valuesCount);
-
-            // Assert.
-            Console.WriteLine(collisionsProbability);
-            Console.WriteLine(expectedCollisionProbability);
-            randomValues.Should().OnlyHaveUniqueItems();
-        }
-
-        private static int FindCollisionsCount(IEnumerable<T> values) =>
-            values.GroupBy(x => x).Where(g => g.Count() > 1).Count();
-
-        private static IDictionary<T, int> FindCollisions(IEnumerable<T> values) => values.GroupBy(x => x)
-            .Where(g => g.Count() > 1)
-            .ToDictionary(x => x.Key, y => y.Count());
 
         [Test]
         public async Task WhenAsyncGettingRandomValue_ThenResultIsNotNull(
